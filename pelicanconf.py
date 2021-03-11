f61031e4bf671d1cefa5ad4138398bd7028b5e80
40823109 <40823109@gm.nfu.edu.tw>
2021-03-11 13:24:16 +0800

    blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = '40823109r'
+AUTHOR = '40823109'
 SITENAME = 'cd2021 40823109'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
